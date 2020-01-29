6bfdab4692dd37d56c52ca3edd55b2beb159f2fc
Carlos Daneil Rigoni <danrigoni1982@gmail.com>
2020-01-29 11:24:03 -0300

    feat: interface cards

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Discription,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Balance</Title>
+            <Discription>$ 2.197.602,65</Discription>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transfer of 200.000,00 $ from Bill Gates today at 06:00 am
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -5,4 +5,58 @@ export const Container = styled.View`
   flex: 1;
   background: #81259d;
   padding-top: ${getStatusBarHeight()}px;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 400px;
+  z-index: 5;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background: #fff;
+  border-radius: 4px;
+  margin: 0 20px;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
+
+export const Discription = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
+
+export const CardFooter = styled.View`
+  padding: 30px;
+  background: #eee;
+  border-radius: 4px;
+`;
+
+export const Annotation = styled.Text`
+  font-size: 12px;
+  color: #333;
 `;
